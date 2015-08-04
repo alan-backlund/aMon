50feea01d386c60e0ff04385a1841e4f4054d91f
Alan Backlund <alan.backlund@users.noreply.github.com>
2015-08-04 14:02:44 -0400

    Add documentation.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Another small system monitor.
 
 This is a small command line program for minimal resource single board computers. It is intended to aid experimentation and testing of boards based of processors like Atmel's AVRs and ST's STM32s.
 
-It features limited line editing (backspace), command history, registers, argument parsing and number formating.
+It features limited line editing (backspace), command history, registers, argument parsing and number formating. For more detail build and view the doxygen documentation.
 
 Building requires:
 re2c - "A tool for writing very fast and very flexible scanners." is available at 'http://re2c.org/'.
--- main.c
+++ main.c
@@ -55,6 +55,49 @@
  * intended primarily test tool for devices containing a small (<64k
  * flash) MPU.
  *
+ * \section command_sec Commands
+ *
+ * Ten commands are included in the monitor: <br/>
+ *     prompt(s) - Select the prompt for input <br/>
+ *       set(cs) - Set register to string <br/>
+ *        get(c) - Display register <br/>
+ *       add(dd) - Add two numbers <br/>
+ *       sub(dd) - Subtract two numbers <br/>
+ *       mul(dd) - Multiply two numbers <br/>
+ *         hex() - Toggle output base <br/>
+ *      echo(s+) - Display parameter <br/>
+ *        help() - Display this help <br/>
+ *        exit() - Exit monitor <br/>
+ * brackets are optional, parameters must be seperated by comma's or space's.
+ * 's' parameters are strings. Quotes are required if they contains spaces.
+ * 'c' parameters are register names, by default a to h.
+ * 'd' parameters are numbers. hexadecimal numbers must have a leading '0x'.
+ * '+' after a parameter indicates that it can be repeated. echo takes one of more strings as parameters.
+ * commands may return a value which the monitor will print (get, add, sub, mul and
+ * echo do this), and/or the may print their output directly (hex and echo do this).
+ * A parameter can be replaced by a command in a string by preceding it with a '!'.
+ * Example: To set register a to the sum of 3 and 5 enter the command <b>set a !"add 3 5"</b>.
+ *
+ * \section addCmd_sec Adding commands
+ *
+ * To add a command named 'newcmd'.
+ * In the commands.c module:
+ * Add a <b>MK_CMD(newcmd);</b> with the other function definitions.
+ * Add a <b>CMD(newcmd, "parameter codes", "Command description")</b> entry to the dsp_table.
+ * In the commands.c module or, preferably, a new module:
+ * Add a function <b>token_t* cmd_newcmd(token_t *args[], int nArgs)</b> to implement the new command.
+ *
+ * \section regiter_sec Registers
+ *
+ * Registers are places where a string or a number can be3 kept for later use.
+ * There are two commands for accessing registers: set and get. 'set' takes the name
+ * of a register and a value (string or number) and copies the value into the
+ * register. 'get' returns the value of a given register which the monitor displays.
+ * The value of a register can be inserted into a command by prefixing the
+ * name of the register with a $ sign.
+ * \note <b>get a</b> is equivalent to <b>echo $a</b>.
+ * \note To increment register a enter <b>set a !"add $a 1"</b>.
+ *
  * \section install_sec Installation
  *
  * All the c, h & re2c files here except main.h and main.c are part of
@@ -62,8 +105,11 @@
  * version" that runs in a unix environment. 
  * 
  * \subsection build_sec Building
- * Just make.<BR/>
- * There are four flags defined in the makefile:<br/>
+ * Two non-standard tools are required:<br/>
+ * re2c - "A tool for writing very fast and very flexible scanners." is available at 'http://re2c.org/'.<br/>
+ * unifdef - "A utility selectively processes conditional C preprocessor #if and #ifdef directives." is available at 'http://dotat.at/prog/unifdef/'.<br/>
+ * Once the preceding tools are installed just make.<br/>
+ * There are four flags defined in the makefile, they are included to allow shrinking the monitor when flash or ram are in short supply:<br/>
  * <b>INCL_REG</b> Include support for registers <br/>
  * <b>INCL_EXIT</b> Include "exit" command.<br/>
  * <b>INCL_MATH</b> Include math commands (add, sub & mul).<br/>
